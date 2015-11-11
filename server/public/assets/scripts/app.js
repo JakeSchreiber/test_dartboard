bff3a5875344a047213b4c249ac04629f0f2287a
JakeSchreiber <jake@jakeschreiber.com>
2015-11-10 20:21:19 -0600

    Add Round of Darts Functionality

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -3,41 +3,61 @@ var roundScore = 0;
 var dartsRemaining = 3;
 
 $(document).ready(function(){
-
-
-	// $(".triple12").on('click', function(){
-	// 	score += 36;
-	// 	$("#score").text(score);
-	// });
-
-	// $(".triple5").on('click', function(){
-	// 	score += 15;
-	// 	$("#score").text(score);
-	// });
-
- $("#boardContainer").on('click', "#btn", function(){
-        var value = $(this).data('value');
-        roundScore += value;
-        console.log(roundScore);
-        score -= value;
-        $("#score").text(score);
-        dartsRemaining--;
-        $("#dartsRemaining").text("Darts Remaining: " + dartsRemaining);
-        for(var i = 0; i <= dartsRemaining; i++){
-        	$("#dart" + [i + 1]).text(roundScore);
-        }
-    });
+    roundOfDarts();
 
 
 
-	// $("#boardContainer").load('Dartboard_without_White_Segments.svg', function(response){
-	// 	console.log("Working");
-	// 	$(this).addClass("svgLoaded");
-		
-	// 	if(!response){
-	// 		// Error loading SVG!
-	// 		// Make absolutely sure you are running this on a web server or localhost!
-	// 	}
-
-	// });
 });
+
+
+function roundOfDarts(){
+    for(var i = 0; i < dartsRemaining; i++){
+
+    }
+    throwDart();
+
+}
+
+function throwDart() {
+    var el = 0;
+    $("#boardContainer").on('click', "#btn", function () {
+        var value = $(this).data('value');
+
+            roundScore += value;
+            score -= value;
+            dartsRemaining--;
+
+        updateScoreboard(score, dartsRemaining);
+        el++;
+        $("#dart" + el).append(value);
+
+        if(el == 3){
+            updateRoundScore(roundScore);
+            el = 0;
+            roundScore = 0;
+            dartsRemaining = 3;
+            alert("Remove Darts");
+            $("#dart1").text("Dart 1: ");
+            $("#dart2").text("Dart 2: ");
+            $("#dart3").text("Dart 3: ");
+
+        }
+
+
+
+    });
+}
+
+function updateScoreboard(score, dartsRemaining){
+    $("#score1").text(score); //Append DOM call function
+    $("#dartsRemaining").text("Darts Remaining: " + dartsRemaining);
+}
+
+function updateRoundScore(roundScore){
+    $("#roundScore").append("</br>" + roundScore);
+    $("#dartsRemaining").text("Darts Remaining: 3");
+
+}
+//for(var i = 0; i <= dartsRemaining; i++){
+//       $("#dart" + [i + 1]).text(roundScore);
+//}
